350aadf370b62e0a932b6bb3ce3aeb4357f264e9
demonkiller <subhadravishwakarma8gris@gmail.com>
2021-01-29 17:40:59 +0530

    ref changed

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ set -e -u
 sudo chown -R root airootfs/
 sudo chgrp -R root airootfs/
 
-iso_name=archlinux
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <http://www.archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_name=thakuros
+iso_label="THAKUR_$(date +%Y%m)"
+iso_publisher="Thakur OS <http://www.thakuros.org>"
+iso_application="Thakur OS Live/Rescue CD"
 iso_version=$(date +%Y.%m.%d)
 install_dir=arch
 work_dir=work
